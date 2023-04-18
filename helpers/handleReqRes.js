d6cff50a62ebf8c71772feecc8972ea21a83d2d0
shohaghpikt <85873344+shohaghpikt@users.noreply.github.com>
2023-04-18 15:18:09 +0600

    start lession 9

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -40,24 +40,23 @@ handler.handleReqRes = (req, res) => {
     // find route
     const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
 
-    // chosenHandler call
-    chosenHandler(requestProperties, (statusCode, payload) => {
-        statusCode = typeof statusCode === 'number' ? statusCode : 500;
-        payload = typeof payload === 'object' ? payload : {};
-
-        const payloadString = JSON.stringify(payload);
-
-        // return the final response
-        res.writeHead(statusCode);
-        res.end(payloadString);
-    });
-
     req.on('data', (buffer) => {
         realData += decoder.write(buffer);
     });
 
     req.on('end', () => {
         realData += decoder.end();
+        // chosenHandler call
+        chosenHandler(requestProperties, (statusCode, payload) => {
+            statusCode = typeof statusCode === 'number' ? statusCode : 500;
+            payload = typeof payload === 'object' ? payload : {};
+
+            const payloadString = JSON.stringify(payload);
+
+            // return the final response
+            res.writeHead(statusCode);
+            res.end(payloadString);
+        });
         console.log(realData);
         // response handle
         // res.end('hello world');
--- index.js
+++ index.js
@@ -10,19 +10,16 @@
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environment');
 // app object - module scaffolding
 const app = {};
-
-// configuration
-app.config = {
-    port: 3000,
-};
 
 // create server
 app.createServer = () => {
     const server = http.createServer(handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`Environtment variable is ${process.env.NODE_ENV}`);
+        console.log(`listening to port ${environment.port}`);
     });
 };
 
