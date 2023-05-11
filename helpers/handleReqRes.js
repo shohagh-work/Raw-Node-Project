bda18b5232af788eaee5f8e0dee20b331ee4ea99
shohaghpikt <85873344+shohaghpikt@users.noreply.github.com>
2023-05-11 16:28:50 +0600

    updated from office

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -49,7 +49,6 @@ handler.handleReqRes = (req, res) => {
         realData += decoder.end();
 
         requestProperties.body = parseJSON(realData);
-        console.log(requestProperties.body);
 
         chosenHandler(requestProperties, (statusCode, payload) => {
             statusCode = typeof statusCode === 'number' ? statusCode : 500;
